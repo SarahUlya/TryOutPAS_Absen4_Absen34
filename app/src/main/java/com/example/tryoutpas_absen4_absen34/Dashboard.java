71459345f17284aaa2bae555f35f290ff6e4dd13
nathnia <05540@smkrus.sch.id>
2025-05-19 15:03:20 +0700

    feat : [AND] new api premiere

--- app/src/main/java/com/example/tryoutpas_absen4_absen34/Dashboard.java
+++ app/src/main/java/com/example/tryoutpas_absen4_absen34/Dashboard.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +15,19 @@ import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Dashboard extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private TeamAdapter teamAdapter;
     DrawerLayout drawerLayout;
     ImageView menu;
     LinearLayout home,dashboard,notification;
@@ -26,6 +37,12 @@ public class Dashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Panggil API
+        fetchTeams();
 
         drawerLayout = findViewById(R.id.drawerLayout);
         menu = findViewById(R.id.menu);
@@ -81,4 +98,28 @@ public class Dashboard extends AppCompatActivity {
         super.onPause();
         closeDrawer(drawerLayout);
     }
+
+    private void fetchTeams() {
+        ApiService apiService = ApiClient.getRetrofitInstance().create(ApiService.class);
+        Call<TeamResponse> call = apiService.getTeams("English Premier League");
+
+        call.enqueue(new Callback<TeamResponse>() {
+            @Override
+            public void onResponse(Call<TeamResponse> call, Response<TeamResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<Team> teams = response.body().getTeams();
+                    teamAdapter = new TeamAdapter(teams);
+                    recyclerView.setAdapter(teamAdapter);
+                } else {
+                    Toast.makeText(Dashboard.this, "Gagal ambil data", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TeamResponse> call, Throwable t) {
+                Toast.makeText(Dashboard.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
+
